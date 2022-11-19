294411403fbedf7dbba139d4bdc76725e25ef190
yoon <beatus96@naver.com>
2022-11-19 18:56:29 +0900

    feat(Input) : 다리의 길이 입력 받기 기능 추가
    양수가 아니면 예외를 발생시킨다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,15 +1,33 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
- * 사용자로부터 입력을 받는 역할을 한다.
+ * 사용자로부터 입력을 받는 역할을 한다. 숫자 아니면 예외발생
  */
 public class InputView {
 
+    private static final String ERROR_MESSAGE_FOR_NOT_NUMBER ="[ERROR] 숫자를 입력해주세요.";
+
     /**
-     * 다리의 길이를 입력받는다.
+     * 다리의 길이를 입력받는다. +
      */
     public int readBridgeSize() {
-        return 0;
+        String input = Console.readLine().trim();
+
+        return castToInt(input);
+    }
+
+    //서비스로직
+    public int castToInt(String input) {
+        if(isPositiveNumber(input)) {
+            return Integer.parseInt(input);
+        }
+        throw new IllegalArgumentException(ERROR_MESSAGE_FOR_NOT_NUMBER);
+    }
+
+    public boolean isPositiveNumber(String input) {
+        return input.chars().allMatch(Character::isDigit); //음수면 false
     }
 
     /**
