76a3c8e5907332392b4ef7e0f2b97316550b3ab8
yoon <beatus96@naver.com>
2022-11-21 07:28:22 +0900

    feat(BridgeRule) : 게임룰 상수클래스 추가

--- src/main/java/bridge/BridgeRule.java
+++ src/main/java/bridge/BridgeRule.java
@@ -1,9 +1,21 @@
 package bridge;
 
 public class BridgeRule {
-    static final int DOWN_BRIDGE_NUMBER = 0;
-    static final String DOWN_BRIDGE_SYMBOL = "D";
+    public static final int DOWN_BRIDGE_NUMBER = 0;
+    public static final String DOWN_BRIDGE_SYMBOL = "D";
 
-    static final int UP_BRIDGE_NUMBER = 1;
-    static final String UP_BRIDGE_SYMBOL = "U";
+    public static final int UP_BRIDGE_NUMBER = 1;
+    public static final String UP_BRIDGE_SYMBOL = "U";
+
+    public static final int BRIDGE_LENGTH_MIN = 3;
+    public static final int BRIDGE_LENGTH_MAX = 20;
+
+    public static final String MOVABLE_SYMBOL  = "O";
+    public static final String UNMOVABLE_SYMBOL  = "X";
+
+    public static final String COMMAND_RETRY  = "R";
+    public static final String COMMAND_QUIT  = "Q";
+
+    public static final String RESULT_SUCCESS  = "성공";
+    public static final String RESULT_FAIL  = "실패";
 }
