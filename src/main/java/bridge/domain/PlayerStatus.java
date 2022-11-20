f80d49f2141522e44461a4cdb0a332e62d07df24
yoon <beatus96@naver.com>
2022-11-21 08:36:04 +0900

    refactor(PlayerStatus) : enum 이용해 플레이어 이동 상태의 기능 수정

--- src/main/java/bridge/domain/PlayerStatus.java
+++ src/main/java/bridge/domain/PlayerStatus.java
@@ -1,42 +1,40 @@
 package bridge.domain;
 
-import java.util.List;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.StringJoiner;
+import java.util.*;
 
-import static bridge.BridgeRule.DOWN_BRIDGE_SYMBOL;
-import static bridge.BridgeRule.UP_BRIDGE_SYMBOL; //ENUM으로 활용 가능
 import static bridge.BridgeRule.MOVABLE_SYMBOL;
 import static bridge.BridgeRule.UNMOVABLE_SYMBOL;
 
+
 public class PlayerStatus {
     private String movingResultSymbol;
-    private Map<String, List<String>> playerStatusMap;
+    private Map<Bridge, List<String>> playerStatusMap;
 
     public void setDefault() {
         this.playerStatusMap = new HashMap<>();
-        playerStatusMap.put(DOWN_BRIDGE_SYMBOL, new ArrayList<>());
-        playerStatusMap.put(UP_BRIDGE_SYMBOL, new ArrayList<>());
+        Arrays.stream(Bridge.values()).forEach(bridge -> {
+            playerStatusMap.put(bridge, new ArrayList<>());
+        });
     }
 
-    /** update player's status*/
+    /**
+     * update player's status
+     */
     public void update(boolean movingResult, int location, String playerMoving) {
         this.movingResultSymbol = getMovingResultSymbol(movingResult);
         updatePlayerStatusMap(location, playerMoving);
     }
 
     public void updatePlayerStatusMap(int location, String playerMoving) {
-        for (String eachBridge : playerStatusMap.keySet()) {
+        for (Bridge eachBridge : playerStatusMap.keySet()) {
             addStatusToEachBridge(eachBridge, location, playerMoving);
         }
     }
 
-    public void addStatusToEachBridge(String eachBridge, int location, String playerMoving) {
+    public void addStatusToEachBridge(Bridge eachBridge, int location, String playerMoving) {
         List<String> eachBridgeStatus = playerStatusMap.get(eachBridge);
 
-        if (playerMoving.equals(eachBridge)) {
+        if (playerMoving.equals(eachBridge.getSymbol())) {
             eachBridgeStatus.add(location, movingResultSymbol);
             return;
         }
@@ -50,22 +48,27 @@ public class PlayerStatus {
         return UNMOVABLE_SYMBOL;
     }
 
-    /** toString */
+    /**
+     * toString
+     */
     @Override
     public String toString() {
-        String upBridgeStatus = getEachBridgeStatus(UP_BRIDGE_SYMBOL);
-        String downBridgeStatus = getEachBridgeStatus(DOWN_BRIDGE_SYMBOL);
-
-        return upBridgeStatus + "\n" + downBridgeStatus;
+        return String.join("\n", getPlayerStatus());
     }
 
-    public String getEachBridgeStatus(String eachBridge) {
-        List<String> eachBridgeStatus = playerStatusMap.get(eachBridge);
-        return eachStatusToString(eachBridgeStatus);
+    public List<String> getPlayerStatus() {
+        List<String> playerStatus = new ArrayList<>();
+
+        for (Bridge eachBridge : Bridge.values()) {
+            List<String> eachBridgeStatus = playerStatusMap.get(eachBridge);
+            playerStatus.add(statusToString(eachBridgeStatus));
+        }
+
+        return playerStatus;
     }
 
     //util 로직
-    public String eachStatusToString(List<String> eachBridgeStatus) {
+    public String statusToString(List<String> eachBridgeStatus) {
         StringJoiner sj = new StringJoiner(" | ", "[ ", " ]");
 
         for (String step : eachBridgeStatus) {
