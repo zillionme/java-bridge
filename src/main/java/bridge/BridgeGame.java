3e3287a39d1398935da323ee8fac4bddaeafc0ee
yoon <beatus96@naver.com>
2022-11-21 04:31:21 +0900

    refactor(BridgeGame) : 게임 재시작 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -28,23 +28,13 @@ public class BridgeGame {
 
         return playerMoving.equals(bridgeMoving);
     }
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
     public void move(String playerMoving) {
         boolean movingResult = judge(playerMoving);
         player.updatePlayerStatus(movingResult, playerMoving);
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public void retry() {
+        player.retry();
     }
-
-
 }
