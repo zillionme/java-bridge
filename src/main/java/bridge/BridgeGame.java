f3dcc0fa1c93cd41e4107aeb9ece5cbf0872cd0f
yoon <beatus96@naver.com>
2022-11-20 17:03:18 +0900

    feat(BridgeGame) : 플레이어 이동 상태 업데이트 기능 추가
    - 각 다리의 이동 상태 업데이트 기능 추가
    - 플레이어 이동 결과 Symbol로 받는 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -12,6 +12,7 @@ public class BridgeGame {
     private final List<String> bridge;
     private Map<String, List<String>> playerStatusMap;
     private int pointer;
+    private boolean movingResult;
 
     public BridgeGame(List<String> bridge) {
         this.bridge = bridge;
@@ -30,21 +31,42 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(String input) {
-        Validator.validateMovingInput(input);
-
-        String playerStep = input;
-        String bridgeStep = bridge.get(pointer);
-
-        String judgeOX = judgePlayerStep(playerStep, bridgeStep);
+    public void move(String playerMoving) {
     }
 
-    public String judgePlayerStep(String playerStep, String bridgeStep) {
-        if(playerStep.equals(bridgeStep)){
+    public boolean judgePlayerMoving(String playerMoving) {
+        String bridgeMoving = bridge.get(pointer);
+
+        if(playerMoving.equals(bridgeMoving)){
+            return true;
+        }
+        return false;
+    }
+
+    public void updatePlayerStatusMap(String playerMoving) {
+
+        for(String eachBridge : playerStatusMap.keySet()) {
+            addStatusToEachBridge(eachBridge, playerMoving);
+        }
+    }
+
+    public void addStatusToEachBridge(String eachBridge, String playerMoving) {
+        List<String> eachBridgeStatus = playerStatusMap.get(eachBridge);
+
+        if (playerMoving.equals(eachBridge)) {
+            eachBridgeStatus.add(pointer, getMovingResultSymbol());
+        }
+
+        eachBridgeStatus.add(pointer, " ");
+    }
+
+    public String getMovingResultSymbol() {
+        if(movingResult) {
             return MOVABLE_SYMBOL;
         }
         return UNMOVABLE_SYMBOL;
     }
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
