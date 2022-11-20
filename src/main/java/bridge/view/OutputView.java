3e270a74205c119b86f99baa70e847739cbda35c
yoon <beatus96@naver.com>
2022-11-21 05:45:32 +0900

    refactor(OutputView) : 플레이어 시도 횟수 출력 기능 수정
    + 성공 여부 출력 기능 수정
    + 주석 제거

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -18,20 +18,12 @@ public class OutputView {
     public void printStart() {
         System.out.println(MESSAGE_TO_OUTPUT_START);
     }
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
     public void printMap(BridgeGame bridgeGame) {
         System.out.println(bridgeGame.getPlayerStatus());
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+
     public void printResult(BridgeGame bridgeGame) {
         System.out.println(MESSAGE_TO_OUTPUT_FINAL_RESULT);
 
@@ -49,7 +41,7 @@ public class OutputView {
     }
 
     public void printTrtCount(BridgeGame bridgeGame) {
-        System.out.println(MESSAGE_TO_OUTPUT_RESULT_TRYCOUNT+ bridgeGame.getTryCount());
+        System.out.println(MESSAGE_TO_OUTPUT_RESULT_TRYCOUNT+ bridgeGame.getPlayerTryCount());
     }
 
     public void printError(String errorMessage) {
