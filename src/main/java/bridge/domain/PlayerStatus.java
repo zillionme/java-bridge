0efc6156d6c12c6569ceea2ba3e7a885bf375fe5
yoon <beatus96@naver.com>
2022-11-21 04:00:38 +0900

    refactor(PlayerStatus) : 플레이어 상태 클래스toString() 오버라이딩

--- src/main/java/bridge/domain/PlayerStatus.java
+++ src/main/java/bridge/domain/PlayerStatus.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.StringJoiner;
 
 import static bridge.BridgeRule.DOWN_BRIDGE_SYMBOL;
 import static bridge.BridgeRule.UP_BRIDGE_SYMBOL; //ENUM으로 활용 가능
@@ -47,6 +48,30 @@ public class PlayerStatus {
             return MOVABLE_SYMBOL;
         }
         return UNMOVABLE_SYMBOL;
+    }
+
+    /** toString */
+    @Override
+    public String toString() {
+        String upBridgeStatus = getEachBridgeStatus(UP_BRIDGE_SYMBOL);
+        String downBridgeStatus = getEachBridgeStatus(DOWN_BRIDGE_SYMBOL);
+
+        return upBridgeStatus + "\n" + downBridgeStatus;
+    }
+
+    public String getEachBridgeStatus(String eachBridge) {
+        List<String> eachBridgeStatus = playerStatusMap.get(eachBridge);
+        return eachStatusToString(eachBridgeStatus);
+    }
+
+    //util 로직
+    public String eachStatusToString(List<String> eachBridgeStatus) {
+        StringJoiner sj = new StringJoiner(" | ", "[ ", " ]");
+
+        for (String step : eachBridgeStatus) {
+            sj.add(step);
+        }
+        return sj.toString();
     }
 
 }
