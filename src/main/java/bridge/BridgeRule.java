5129f7fae853df9f83aa2cc38bcc317fd3c6da5c
yoon <beatus96@naver.com>
2022-11-20 13:27:20 +0900

    feat(BridgeRule) : 다리 게임 상수 클래스 추가
    다리 게임에 필요한 상수를 관리한다.

---
+++ src/main/java/bridge/BridgeRule.java
@@ -0,0 +1,9 @@
+package bridge;
+
+public class BridgeRule {
+    static final int DOWN_BRIDGE_NUMBER = 0;
+    static final String DOWN_BRIDGE_SYMBOL = "D";
+
+    static final int UP_BRIDGE_NUMBER = 1;
+    static final String UP_BRIDGE_SYMBOL = "U";
+}
