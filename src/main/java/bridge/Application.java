69f4613bfb52525edcb279b3a10392d6ddb9a495
yoon <beatus96@naver.com>
2022-11-21 07:28:57 +0900

    feat : 3차 refactor 완료
    - 초안
    - 1,2차 refactor 저장을 안함...

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,11 @@
 package bridge;
 
+import bridge.controller.Controller;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller();
+        controller.generate();
     }
 }
