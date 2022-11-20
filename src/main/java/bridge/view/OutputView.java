4062a39011941292057201747bf8e8ebb4569de2
yoon <beatus96@naver.com>
2022-11-21 05:41:32 +0900

    refactor(Output) : 플레이어 이동 상태 출력 기능 수정

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -24,7 +24,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap(BridgeGame bridgeGame) {
-        System.out.println(bridgeGame.getPlayerStatusMap());
+        System.out.println(bridgeGame.getPlayerStatus());
     }
 
     /**
