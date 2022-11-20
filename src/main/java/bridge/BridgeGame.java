e05d006ff913507171079f101c2244a8bcc83055
yoon <beatus96@naver.com>
2022-11-20 15:57:00 +0900

    feat(BridgeGame) : 다리 게임 초기화 기능 추가
    - 포인터 초기화
    - 플레이어 맵 초기화

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,36 @@
 package bridge;
 
+import java.util.*;
+
+import static bridge.BridgeRule.*;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final List<String> bridge;
+    private Map<String, List<String>> playerStatusMap;
+    private int pointer;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        setDefaultPlayerStatus();
+    }
+
+    public void setDefaultPlayerStatus() {
+        this.pointer = 0;
+        this.playerStatusMap = new HashMap<>();
+        playerStatusMap.put(DOWN_BRIDGE_SYMBOL, new ArrayList<>());
+        playerStatusMap.put(UP_BRIDGE_SYMBOL, new ArrayList<>());
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String input) {
+
     }
 
     /**
@@ -20,4 +40,6 @@ public class BridgeGame {
      */
     public void retry() {
     }
+
+
 }
