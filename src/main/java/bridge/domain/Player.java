f91991ac41c0a8cf959e26c4f359fbac12453fe2
yoon <beatus96@naver.com>
2022-11-21 04:06:36 +0900

    refactor(Player) : 생성자 및 디폴트 값으로 세팅하는 기능 추가
    - 플레이어의 상태 = 디폴트
    - 플레이어의 위치 = 0

--- src/main/java/bridge/domain/Player.java
+++ src/main/java/bridge/domain/Player.java
@@ -4,4 +4,13 @@ public class Player {
     private PlayerStatus playerStatus = new PlayerStatus();
     private int location;
     private int tryCount = 1;
+
+    public Player() {
+        setDefault();
+    }
+
+    public void setDefault() {
+        location = 0;
+        playerStatus.setDefault();
+    }
 }
