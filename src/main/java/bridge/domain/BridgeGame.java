da389832dfbd6a667b865a477d9e3efd5c61cd18
yoon <beatus96@naver.com>
2022-11-21 08:50:45 +0900

    refactor : domain 패키지로 이동

---
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -0,0 +1,57 @@
+package bridge.domain;
+
+import java.util.List;
+
+/**
+ * 다리 건너기 게임을 관리하는 클래스
+ */
+public class BridgeGame {
+    private final List<String> bridge;
+    private final Player player;
+    private boolean isPlaying;
+
+    public BridgeGame(List<String> bridge, Player player) {
+        this.bridge = bridge;
+        this.player = player;
+        this.isPlaying = true;
+    }
+
+
+    public boolean judge(String playerMoving) {
+        return player.compare(playerMoving, bridge);
+    }
+
+    public void move(String playerMoving) {
+        boolean movingResult = judge(playerMoving);
+        player.updatePlayerStatus(movingResult, playerMoving);
+    }
+
+    public void retry() {
+        player.retry();
+    }
+
+    public void quit() {
+        isPlaying = false;
+    }
+
+//    public void execute(String command) {
+//
+//    }
+
+    // 게임 결과 (enum으로 처리가능) / movingResult도 저장할 수 있으면 좋을듯 하다.
+    public boolean isSuccessfullyCompleted(){
+        return player.isArrived(bridge.size());
+    }
+    public boolean isCompletedOrStopped() {
+        return isSuccessfullyCompleted() || !isPlaying;
+    }
+
+    public String getPlayerStatus() {
+        return player.getPlayerStatus();
+    }
+
+    public int getPlayerTryCount() {
+        return player.getTryCount();
+    }
+
+}
