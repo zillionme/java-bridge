8eca2345cde4a5663a969b7894a687d036612950
yoon <beatus96@naver.com>
2022-11-20 18:10:53 +0900

    feat(Output) : 게임 최종 결과 출력 기능 추가
    - 게임 이동 결과
    - 게임 성공 여부
    - 게임 시도 횟수

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import static bridge.BridgeRule.RESULT_FAIL;
+import static bridge.BridgeRule.RESULT_SUCCESS;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -27,6 +30,24 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(BridgeGame bridgeGame) {
+        System.out.println(MESSAGE_TO_OUTPUT_FINAL_RESULT);
+
+        printMap(bridgeGame);
+        printSuccessOrFail(bridgeGame);
+        printTrtCount(bridgeGame);
     }
+
+    public void printSuccessOrFail(BridgeGame bridgeGame) {
+        if (bridgeGame.isSuccessfullyCompleted()) {
+            System.out.println(MESSAGE_TO_OUTPUT_RESULT_SUCCESS + RESULT_SUCCESS);
+            return;
+        }
+        System.out.println(MESSAGE_TO_OUTPUT_RESULT_SUCCESS + RESULT_FAIL);
+    }
+
+    public void printTrtCount(BridgeGame bridgeGame) {
+        System.out.println(MESSAGE_TO_OUTPUT_RESULT_TRYCOUNT+ bridgeGame.getTryCount());
+    }
+
 }
