25f2c003aa7ab37cc6d955fe98ad457af05ef7d3
yoon <beatus96@naver.com>
2022-11-20 16:12:19 +0900

    feat(Validator) : 이동할 칸 입력값 유효성 체크 기능 추가
    - 유효하지 않으면 예외발생

---
+++ src/main/java/bridge/Validator.java
@@ -0,0 +1,14 @@
+package bridge;
+
+import static bridge.BridgeRule.*;
+import static bridge.BridgeRule.UP_BRIDGE_SYMBOL;
+import static bridge.Exception.ERROR_MESSAGE_INVALID_MOVE;
+
+public class Validator {
+
+    public static void validateMovingInput (String input) {
+        if(!input.equals(DOWN_BRIDGE_SYMBOL) && !input.equals(UP_BRIDGE_SYMBOL)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_MOVE);
+        }
+    }
+}
