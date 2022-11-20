3c5ba1b75765a90f9bde2483ace67e816cd70cd3
yoon <beatus96@naver.com>
2022-11-20 18:47:39 +0900

    feat(Controller) : 게임 플레이 기능 추가
    - 이동할 칸 입력받기
    - 이동하기
    - 이동 결과 출력하기
    - 틀린 경우, 계속 지속할지 여부 결정하기*(추가예정)

--- src/main/java/bridge/Controller.java
+++ src/main/java/bridge/Controller.java
@@ -1,17 +1,25 @@
 package bridge;
 
-import java.util.*;
+import java.util.List;
+
+import static bridge.BridgeRule.COMMAND_QUIT;
+import static bridge.BridgeRule.COMMAND_RETRY;
 
 public class Controller {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
-    private final Validator validator = new Validator();
     private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
     public void generate() {
-            outputView.printStart();
-            BridgeGame bridgeGame = createBridgeGame();
+        outputView.printStart();
+        BridgeGame bridgeGame = createBridgeGame();
+
+        //최종 결과가 false이고, 게임 중인 동안만
+        while (true) {
+            playBridgeGame(bridgeGame);
+        }
+
     }
 
     public BridgeGame createBridgeGame() {
@@ -19,6 +27,12 @@ public class Controller {
         List<String> bridge = bridgeMaker.makeBridge(size);
 
         return new BridgeGame(bridge);
+    }
+
+    public void playBridgeGame(BridgeGame bridgeGame) {
+        String playerMoving = inputView.readMoving();
+        bridgeGame.move(playerMoving);
+        outputView.printMap(bridgeGame);
     }
 
 }
