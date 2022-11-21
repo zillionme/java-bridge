2efdf91b357cb5ffeee87f4483cc3111178b6657
yoon <beatus96@naver.com>
2022-11-22 06:22:14 +0900

    test(GameValidator) : 다리 길이 넘어 이동 시, 예외 발생 테스트

--- src/test/java/bridge/util/validation/GameValidatorTest.java
+++ src/test/java/bridge/util/validation/GameValidatorTest.java
@@ -2,7 +2,12 @@ package bridge.util.validation;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -30,6 +35,21 @@ class GameValidatorTest {
     void validateCommandErrorTest(String input) {
         assertThatThrownBy(()->GameValidator.validateCommand(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("플레이어가 다리 길이 이상으로 이동하려고 하면, 예외 발생 테스트")
+    @ParameterizedTest
+    @MethodSource("provideLocation_Bridge")
+    void validatePlayerLocationErrorTest(int location, List<String> bridge) {
+        assertThatThrownBy(()->GameValidator.validatePlayerLocation(location, bridge))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> provideLocation_Bridge() {
+        return Stream.of(
+                Arguments.of(10,List.of("U", "U", "D", "D")),
+                Arguments.of(4,List.of("U", "D", "U", "D"))
+        );
     }
 
 }
