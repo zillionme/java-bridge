4c4b06eed3d9e9ffa69a4430f956658938645177
yoon <beatus96@naver.com>
2022-11-22 08:47:41 +0900

    test(ViewValidator) : 입력값이 null이거나 빈 값인 경우 예외발생 테스트

--- src/test/java/bridge/util/validation/ViewValidatorTest.java
+++ src/test/java/bridge/util/validation/ViewValidatorTest.java
@@ -2,6 +2,7 @@ package bridge.util.validation;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,6 +14,14 @@ class ViewValidatorTest {
     @ValueSource(strings = {"-1","가","A"})
     void isNumberErrorTest(String input) {
         assertThatThrownBy(()->ViewValidator.isNumber(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력 값이 null이거나 빈 값이면, 예외 발생 테스트")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void isNotEmptyOrNullTest(String input) {
+        assertThatThrownBy(()->ViewValidator.isNotEmptyOrNull(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
