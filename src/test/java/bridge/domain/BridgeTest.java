cd9e3593faa8d44fbce258b6940181461e269cd2
yoon <beatus96@naver.com>
2022-11-21 21:38:58 +0900

    test(Bridge) : 입력된 숫자에 해당하는 다리 기호 반환 기능 테스트
    - 예상값 반환하는 지 테스트
    - 입력한 숫자 값이, enum에 없는 경우 예외 발생 테스트

---
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -0,0 +1,28 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BridgeTest {
+
+    @DisplayName("입력한 숫자에 해당하는 다리가 없는 경우, 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {2,11})
+    void getSymbolByExceptionTest(int number) {
+        assertThatThrownBy(()->Bridge.getSymbolBy(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력한 숫자에 해당하는 다리의 기호를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"0,D","1,U"})
+    void getSymbolByTest(int number, String expectedSymbol) {
+        assertThat(Bridge.getSymbolBy(number))
+                .isEqualTo(expectedSymbol);
+    }
+}
