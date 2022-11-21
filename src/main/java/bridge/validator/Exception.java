9fd98e3d3423d85675349f8a149cdc1bd61efa80
yoon <beatus96@naver.com>
2022-11-21 10:11:21 +0900

    feat (Exception) : 예외 상황 상수 클래스 추가
    - validator에서만 사용함

---
+++ src/main/java/bridge/validator/Exception.java
@@ -0,0 +1,9 @@
+package bridge.validator;
+
+public class Exception {
+    public static final String ERROR_MESSAGE_FOR_NOT_NUMBER ="[ERROR] 숫자를 입력해주세요.";
+    public static final String ERROR_MESSAGE_OUT_OF_RANGE ="[ERROR] 3이상 20 이하의 숫자를 입력해주세요.";
+    public static final String ERROR_MESSAGE_INVALID_MOVE ="[ERROR] 이동할 칸이 올바르지 않습니다. (위: U, 아래: D)";
+    public static final String ERROR_MESSAGE_INVALID_COMMAND ="[ERROR] 명령이 올바르지 않습니다. (재시도: R, 종료: Q)";
+
+}
