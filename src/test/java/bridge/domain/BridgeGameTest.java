b2e675fd7ccd02afaa34e54c38d6d4faea715af8
yoon <beatus96@naver.com>
2022-11-22 02:19:56 +0900

    test(BridgeGame) : 이동 기능 테스트
    - 이동 후, 플레이어 상태 업데이트 되는지 확인 테스트
    
    문제점 :
    1. 플레이어의 이전 movingResult가 false이면, 이동하지 않고, 물어봐야함.(이건 그렇다 치고,)
    2. 다리의 size 이상으로, 이동하려고 하면, indexoutBound 예외 발생. player 업데이트 할때, validatePointer 해야한다.

--- src/test/java/bridge/domain/BridgeGameTest.java
+++ src/test/java/bridge/domain/BridgeGameTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -37,9 +40,25 @@ class BridgeGameTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void move() {
+    @DisplayName("이동 후, 플레이어 상태가 업데이트 되는지 확인 테스트")
+    @ParameterizedTest
+    @MethodSource("providePlayerMovings_And_ExpectedStatus")
+    void moveTest(List<String> playerMovings, String expectedStatus) {
+        for(String playingMoving : playerMovings) {
+            bridgeGame.move(playingMoving);
+        }
+        assertThat(bridgeGame.getPlayerStatus()).contains(expectedStatus);
     }
+
+    private static Stream<Arguments> providePlayerMovings_And_ExpectedStatus() {
+        return Stream.of(
+                Arguments.of(List.of("U"), "[ O ]\n[   ]"),
+                Arguments.of(List.of("U","U"), "[ O | O ]\n[   |   ]"),
+                Arguments.of(List.of("U","U","D"), "[ O | O |   ]\n[   |   | X ]"),
+                Arguments.of(List.of("U","U","D","D"), "[ O | O |   |   ]\n[   |   | X | O ]")
+        );
+    }
+
 
     @Test
     void retry() {
