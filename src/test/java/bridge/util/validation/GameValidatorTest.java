3972b07db0f1b35e811b2c3bac8ff97310aeb61b
yoon <beatus96@naver.com>
2022-11-22 06:20:11 +0900

    test(GameValidator) : 입력한 다리 기호 존재 여부 확인 테스트

--- src/test/java/bridge/util/validation/GameValidatorTest.java
+++ src/test/java/bridge/util/validation/GameValidatorTest.java
@@ -16,4 +16,12 @@ class GameValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("이동하려는 다리의 기호가 없으면, 예외 발생 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"K","u","d","UP_BRIDGE"})
+    void validateMovingInputErrorTest(String input) {
+        assertThatThrownBy(()-> GameValidator.validateMovingInput(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
