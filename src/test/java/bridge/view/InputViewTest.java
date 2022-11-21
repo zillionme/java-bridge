4b1b2113c3ecd65194dabcb0da8d699d8039415a
yoon <beatus96@naver.com>
2022-11-22 08:54:16 +0900

    test(Input) : 다리길이 입력값을 숫자로 바꾸는 기능 테스트

---
+++ src/test/java/bridge/view/InputViewTest.java
@@ -0,0 +1,18 @@
+package bridge.view;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputViewTest {
+    private final InputView inputView = new InputView();
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void castToIntErrorTest(String input) {
+        assertThatThrownBy(()->inputView.castToInt(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
