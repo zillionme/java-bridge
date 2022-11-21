ab7dbe57f79565bd2fb864c4b4173a691f7ec40c
yoon <beatus96@naver.com>
2022-11-21 18:57:24 +0900

    feat(Bridge) : 매개변수에 연결되는 enum상수를 반환하는 기능 추가

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import java.util.Arrays;
+
 public enum Bridge {
     UP_BRIDGE("U",1),
     DOWN_BRIDGE("D",0);
@@ -12,15 +14,18 @@ public enum Bridge {
         this.mappingNumber = mappingNumber;
     }
 
-    public boolean isSymbolEqualTo(String input) {
-        return this.symbol.equals(input);
-    }
-
     public String getSymbol() {
         return symbol;
     }
 
     public int getMappingNumber() {
         return mappingNumber;
+    }
+
+    public static String getSymbolBy(int number) {
+        return Arrays.stream(Bridge.values())
+                .filter(bridge -> bridge.getMappingNumber() == number)
+                .findFirst()
+                .get().getSymbol();
     }
 }
