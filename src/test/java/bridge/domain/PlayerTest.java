6df3a8a14640f52cc289c8f48f812930d3820da7
yoon <beatus96@naver.com>
2022-11-22 05:50:20 +0900

    test(Player) : 지정된 다리 끝에 도착 여부 확인 기능 테스트

--- src/test/java/bridge/domain/PlayerTest.java
+++ src/test/java/bridge/domain/PlayerTest.java
@@ -2,8 +2,14 @@ package bridge.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerTest {
@@ -21,4 +27,22 @@ class PlayerTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("플레이어가 다리 끝에 성공적으로 도착했는지 여부 확인 테스트")
+    @ParameterizedTest
+    @MethodSource("provideMovingResult_PlayerMoving_Expected")
+    void isArrivedTest(List<Boolean> movingResult,List<String> playerMoving, boolean expected) {
+        for(int i = 0; i < movingResult.size(); i++) {
+            player.updatePlayerStatus(movingResult.get(i), playerMoving.get(i));
+        }
+
+        assertThat(player.isArrived(bridge.size())).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideMovingResult_PlayerMoving_Expected() {
+        return Stream.of(
+                Arguments.of(List.of(true), List.of("U"),false),
+                Arguments.of(List.of(true, true, true, true), List.of("U", "U", "D", "D"),true),
+                Arguments.of(List.of(true, true, true, false), List.of("U", "U", "D", "U"),false)
+        );
+    }
 }
