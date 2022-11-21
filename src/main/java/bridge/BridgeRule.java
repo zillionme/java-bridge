7f3ec8b10fe94ad4f875260b094648fa9c0ddf32
yoon <beatus96@naver.com>
2022-11-21 15:26:51 +0900

    refactor(BridgeRule) : enum으로 바꾼값 상수클래스에서 삭제

--- src/main/java/bridge/BridgeRule.java
+++ src/main/java/bridge/BridgeRule.java
@@ -1,12 +1,6 @@
 package bridge;
 
 public class BridgeRule {
-    public static final int DOWN_BRIDGE_NUMBER = 0;
-    public static final String DOWN_BRIDGE_SYMBOL = "D";
-
-    public static final int UP_BRIDGE_NUMBER = 1;
-    public static final String UP_BRIDGE_SYMBOL = "U";
-
     public static final int BRIDGE_LENGTH_MIN = 3;
     public static final int BRIDGE_LENGTH_MAX = 20;
 
