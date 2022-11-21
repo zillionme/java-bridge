217fc4304727c6a2f475f07683420ce4e1245baf
yoon <beatus96@naver.com>
2022-11-21 19:02:12 +0900

    reafactor(BridgeRule) : enum클래스로 바꾼 상수값 삭제

--- src/main/java/bridge/BridgeRule.java
+++ src/main/java/bridge/BridgeRule.java
@@ -3,13 +3,8 @@ package bridge;
 public class BridgeRule {
     public static final int BRIDGE_LENGTH_MIN = 3;
     public static final int BRIDGE_LENGTH_MAX = 20;
-
-    public static final String MOVABLE_SYMBOL  = "O";
-    public static final String UNMOVABLE_SYMBOL  = "X";
-
     public static final String COMMAND_RETRY  = "R";
     public static final String COMMAND_QUIT  = "Q";
 
-    public static final String RESULT_SUCCESS  = "성공";
-    public static final String RESULT_FAIL  = "실패";
+
 }
