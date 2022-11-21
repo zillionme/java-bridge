b538faa02840c18530e56bcc580641929cde3a7c
yoon <beatus96@naver.com>
2022-11-22 06:18:35 +0900

    test(GameValidator) : 다리 길이가 범위 밖인 경우 예외 발생 테스트

---
+++ src/test/java/bridge/util/validation/GameValidatorTest.java
@@ -0,0 +1,19 @@
+package bridge.util.validation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class GameValidatorTest {
+
+    @DisplayName("다리 길이가 지정된 범위 밖이면, 예외 발생 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,21,0,-1})
+    void validateBridgeSizeErrorTest(int size) {
+        assertThatThrownBy(()->GameValidator.validateBridgeSize(size))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
