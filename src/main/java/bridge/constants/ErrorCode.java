ec5eda8ad8cd894f2ca50049e22f0bc734353390
yoon <beatus96@naver.com>
2022-11-22 18:19:59 +0900

    refactor(ErrorCode) : 입력 오류 발생 안할 때까지 무한 입력 받음으로써 관련 예외 처리 삭제

--- src/main/java/bridge/constants/ErrorCode.java
+++ src/main/java/bridge/constants/ErrorCode.java
@@ -1,4 +1,4 @@
-package bridge.util.constants;
+package bridge.constants;
 
 public enum ErrorCode {
     ERROR_ULL_OR_EMPTY("값을 입력해주세요."),
@@ -6,7 +6,6 @@ public enum ErrorCode {
     ERROR_INVALID_BRIDGE_SIZE("3이상 20 이하의 숫자를 입력해주세요."),
     ERROR_NO_BRIDGE_MATCHING_NUMBER("숫자에 해당하는 다리가 없습니다."),
     ERROR_INVALID_BRIDGE_TO_MOVE("이동할 칸이 올바르지 않습니다. (위: U, 아래: D)"),
-    ERROR_INVALID_LOCATION_TO_MOVE("다리를 모두 건넜습니다."),
     ERROR_INVALID_COMMAND("명령이 올바르지 않습니다. (재시도: R, 종료: Q)");
 
     private static final String ERROR_BEGIN = "[ERROR] ";
